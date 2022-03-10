7846c6522718a01ac95d3ec50fbf27e4d5dfe14c
Joel Rosas <joeli.rosas20@gmail.com>
2022-03-09 20:59:13 -0500

    Added planned DAO methods

--- src/main/java/com/training/pms/dao/LoginDAO.java
+++ src/main/java/com/training/pms/dao/LoginDAO.java
@@ -10,5 +10,15 @@ public interface LoginDAO {
 	public boolean validate(String userId, String password);
 	public List<User> getUsers();
 	public List<User> getUsersByUserName(String username);
+	//Get username to see whether its an employee or manager
+	//Employee see their pending reimbursement
+	//Employee see their resolved reimbursement
+	//Employee can see their info
+	//Employee can update their info
+	//Manager see all pending reimbursement
+	//Manager see all resolved reimbursement
+	//Manager see request of specific employee
+	//Manager can accept request
+	//Manager can deny request
 	
 }
