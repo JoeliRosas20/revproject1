47e2e4d934bad41eb007dcb5ce56dc88f21f4552
Joel Rosas <joeli.rosas20@gmail.com>
2022-03-10 17:27:15 -0500

    Added test methods in the Junit

--- src/main/java/com/training/pms/dao/LoginDAO.java
+++ src/main/java/com/training/pms/dao/LoginDAO.java
@@ -6,19 +6,22 @@ import com.project.model.User;
 
 public interface LoginDAO {
 	
+	//USER STUFF
 	public boolean register(User user);
 	public boolean validate(String userId, String password);
 	public List<User> getUsers();
 	public List<User> getUsersByUserName(String username);
-	//Get username to see whether its an employee or manager
+	//EMPLOYEE STUFF
 	//Employee see their pending reimbursement
 	//Employee see their resolved reimbursement
 	//Employee can see their info
 	//Employee can update their info
+	//MANAGER STUFF
 	//Manager see all pending reimbursement
 	//Manager see all resolved reimbursement
 	//Manager see request of specific employee
 	//Manager can accept request
 	//Manager can deny request
-	
+	//HELPER
+	//Get username to see whether its an employee or manager
 }
