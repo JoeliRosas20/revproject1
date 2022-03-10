7345e0b2a12675351ee723b2a85335851cfbb2bf
Joel Rosas <joeli.rosas20@gmail.com>
2022-03-10 18:15:50 -0500

    Added more in the Junit

--- src/test/java/com/training/pms/dao/LoginDAOImplTest.java
+++ src/test/java/com/training/pms/dao/LoginDAOImplTest.java
@@ -2,13 +2,28 @@ package com.training.pms.dao;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.project.model.User;
+
 class LoginDAOImplTest {
+	
+	LoginDAO loginDAO = new LoginDAOImpl();
+	User user;
+	String username;
+	String password;
+	String gender;
+	String finalNotification;
+	String qualification;
+	int userId;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -20,15 +35,46 @@ class LoginDAOImplTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		username = "dummy";
+		password = "dummyroot123";
+		gender = "male";
+		finalNotification = "sms";
+		qualification = "graduate";
+		user = new User(-1, username, password, gender, finalNotification, qualification);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 	}
-
+	
+	@Disabled
 	@Test
+	@DisplayName("Testing register")
+	void testRegister() {
+		assertTrue(loginDAO.register(user));
+	}
+	
+	//@Disabled
+	@Test
+	@DisplayName("Testing login")
+	void testValidate() {
+		assertTrue(loginDAO.validate(username, password));
+	}
+	
+	@Disabled
+	@Test
+	@DisplayName("Testing getting all the users")
 	void testGetUsers() {
-		fail("Not yet implemented");
+		List<User> users = loginDAO.getUsers();
+		assertNotEquals(0,users.size());
+	}
+	
+	@Disabled
+	@Test
+	@DisplayName("Testing getting users by name")
+	void testGetUsersByUserName() {
+		List<User> users = loginDAO.getUsersByUserName(username);
+		assertNotEquals(0, users.size());
 	}
 
 }
